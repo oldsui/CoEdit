75bdfcf63314f3c564b4e9af21cecabef41e1f25
oldsui <rayliu317612@gmail.com>
2017-02-06 23:25:29 -0800

    server listen port

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(process.env.PORT ||3000, function(){
+  console.log('listening on *: $PORT or 3000');
 });
 
